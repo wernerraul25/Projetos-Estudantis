1cc99468702a5fc67eaed366a13499450bc033e7
Raul Werner <qrananico25@gmail.com>
2023-07-08 13:48:38 -0300

    bola movimentando

--- pong/main.py
+++ pong/main.py
@@ -2,7 +2,9 @@ import pygame
 
 pygame.init()
 
-tamanho = (1280,720)
+largura = 1280
+altura = 720
+tamanho = (largura,altura)
 verde = (0,128,0)
 cinza = (192,192,192)
 preto = (0,0,0)
@@ -25,6 +27,13 @@ y_jogador2_cima = 265
 y_jogador2_baixo = 454
 subindo2 = False
 descendo2 = False
+
+#bola
+pos_x = largura // 2
+pos_y = altura // 2
+vel_x = 4
+vel_y = 4
+raio = 10
 
 running = True
 while running:
@@ -54,13 +63,13 @@ while running:
     tela.fill(verde)
     
     #desenho do campo
-    pygame.draw.circle(tela,branco,(642,360),84)
-    pygame.draw.circle(tela,verde,(642,360),80)
+    pygame.draw.circle(tela,branco,(largura // 2,altura // 2),84)
+    pygame.draw.circle(tela,verde,(largura // 2,altura // 2),80)
     pygame.draw.line(tela,branco,(0,-2),(0,722),4)
     pygame.draw.line(tela,branco,(1279,-2),(1279,722),4)
     pygame.draw.line(tela,branco,(-2,719),(1282,719),4)
     pygame.draw.line(tela,branco,(-2,1),(1282,1),4)
-    pygame.draw.line(tela,branco,(642,0),(642,720),4)
+    pygame.draw.line(tela,branco,(largura // 2,0),(largura // 2,720),4)
 
     #goleiros e bola
     #jogador 1
@@ -68,7 +77,14 @@ while running:
     #jogador 2
     pygame.draw.line(tela,preto,(x_jogador2,y_jogador2_cima),(x_jogador2,y_jogador2_baixo),5)
     #bola
-    pygame.draw.circle(tela,preto,(642,360),10)
+    pygame.draw.circle(tela,preto,(pos_x,pos_y),raio)
+    pos_x += vel_x
+    pos_y += vel_y
+    
+    if pos_x <= 0 or pos_x >= largura:
+        vel_x = -vel_x
+    if pos_y <= 0 or pos_y >= altura:
+        vel_y = -vel_y
 
     if subindo1:
         y_jogador1_cima -= 8
