f8c51f1a76925b943d3e09775e75be9b26ab72f1
Raul Werner <qrananico25@gmail.com>
2023-07-10 17:31:51 -0300

    Codifica senha local
    Codifica a senha no arquivo, porém executando o programa mostra todas

--- codificador_senhas/main.py
+++ codificador_senhas/main.py
@@ -1,22 +1,47 @@
-master_pwd = input("Qual a senha master? ")
+import os
+from cryptography.fernet import Fernet
 
+def clear_terminal():
+    os.system("cls")
 
+'''def write_key():
+    key = Fernet.generate_key()
+    with open("key.key","wb") as key_file:
+        key_file.write(key)'''
+
+def load_key():
+    file = open("key.key","rb")
+    key = file.read()
+    file.close()
+    return key
+
+clear_terminal()
+#master_pwd = input("Qual a senha master? ")
+key = load_key() #+ master_pwd.encode()
+fer = Fernet(key)
 
 def view():
-    pass
+    with open("passwords.txt","r") as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            plat, user, passw = data.split("|")
+            print("Username:", user, "| Senha:", fer.decrypt(passw.encode()).decode(), "| Plataforma:", plat)
+
 def add():
     username = input("Username: ")
     pwd = input("Senha: ")
     platform = input("Plataforma: ")
-    try:
-        with open("passwords.txt","a") as f:
-            f.write(platform + "|" + username + "|" + pwd + "\n")
-    except:
-        with open("passwords.txt","w") as f:
-            f.write(platform + "|" + username + "|" + pwd + "\n")
 
-choose = str(input("Para adicionar senhas:(1)\nPara ver senhas:(2)"))
-if choose == "1":
-    add()
-elif choose == "2":
-    view()
+    with open("passwords.txt","a") as f:
+        f.write(platform + "|" + username + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
+while True:
+    clear_terminal()
+    choose = str(input("Para adicionar senhas:(add)\nPara ver senhas:(view)\nSair:(q)\n")).lower()
+    if choose == "q":
+        break
+    if choose == "add":
+        add()
+    elif choose == "view":
+        view()
+        input("Pressione qualquer tecla para continuar!")
