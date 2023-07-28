06914c822ccca9af24bfc649a193f6027017d67c
Raul Werner <qrananico25@gmail.com>
2023-07-28 18:17:38 -0300

    Download está funcionando

--- yt_downloader/main.py
+++ yt_downloader/main.py
@@ -5,12 +5,19 @@ link = argv[1]
 
 yt = YouTube(link)
 
-print("Título do vídeo: " , yt.title)
-print("Views: " , yt.views)
-print("Autor: ", yt.author)
+titulo = yt.title
+views = yt.views
+autor = yt.author
 
-'''yd = yt.streams.get_audio_only()  #audio
+print("Título do vídeo: " , titulo)
+print("Views: " , views)
+print("Autor: ", autor)
+
+yd = yt.streams.get_audio_only()  #audio
 yd = yt.streams.get_highest_resolution()  #vídeo
 
-yd.download('/Users/Geral/Desktop/Projetospython/Projetos-Estudantis/yt_downloader')'''
-#*******   erro no DOWNLOAD   ******
+try:
+    yd.download('/Users/Geral/Downloads') #download video
+    print('Download de', '"'+titulo+'"', 'concluído!')
+except:
+    print("Erro no download!")
